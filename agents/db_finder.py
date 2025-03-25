d532852fe963bbda9c2cb738177672254515f410
NawidT <nawidtahmid@gmail.com>
2025-03-25 19:32:43 -0400

    db finder agent

--- agents/db_finder.py
+++ agents/db_finder.py
@@ -1,48 +1,132 @@
 # AGENT SEARCHES KAGGLE API VIA CLI in DOCKER
-import kaggle
+import json
 from langgraph.graph import MessagesState, START, END, StateGraph
 from langchain_core.tools import tool
 from langchain_openai.chat_models import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, BaseMessage
 import subprocess
 from typing_extensions import TypedDict
 import os
+from prompts import kaggle_api_search_prompt, db_finder_plan_search_prompt, db_finder_loop_prompt
 
+chat = ChatOpenAI(model="gpt-4o-mini", api_key=os.environ['OPENAI_API_KEY'])
 
 class DBFinderState(TypedDict):
     messages: list[MessagesState]
     query: str
+    temp: dict
 
-@tool
+
+def agentic_loop(state: DBFinderState) -> str:
+    """
+    This function is used to run the agentic loop. It will select between running kaggle api search, altering the temp dict in the state, or selecting a dataset (END).
+    """
+    
+    while True:
+        # only store the last 10 messages
+        state['messages'] = state['messages'][-10:]
+
+        # run the central message
+        print("------------------------------------------------------- " + str(len(state['messages'])) + " -------------------------------------------------------")
+        # print([str(m.content).strip() for m in state['messages'] if isinstance(m, BaseMessage)])
+        central_msg = HumanMessage(content=
+            db_finder_loop_prompt.format(query=state['query'], temp=" ".join([f"{k}: {v}" for k, v in state['temp'].items()]))
+        )
+
+        temp = state['messages']
+        temp.append(central_msg)
+        result = chat.invoke(temp)
+        state['messages'].pop(len(state['messages']) - 1)
+
+        # cleanup the result
+        if result.content.startswith('```json'):
+            result.content = result.content.replace('```json', '', 1)
+        if result.content.startswith('```'):
+            result.content = result.content.replace('```', '', 1)
+        if result.content.endswith('```'):
+            result.content = result.content.rsplit('```', 1)[0]
+
+        try:
+            result_json = json.loads(result.content.strip())
+        except json.JSONDecodeError:
+            print(result.content.strip())
+            raise ValueError('Invalid JSON')
+    
+        # add the messages to the state in a custom way
+        state['messages'].append(HumanMessage(content="What is your action?"))
+        state['messages'].append(AIMessage(content=str(result_json['action']) + " : " + str(result_json['reason'])))
+        print(result_json['action'] + " : " + result_json['reason'])
+
+        # execute the actions
+        if result_json['action'] == 'plan':
+            state = plan(state)
+        elif result_json['action'] == 'search':
+            state['query'] = result_json['details']
+            state = run_kaggle_api_search(state)
+        elif result_json['action'] == 'alter':
+            state['temp'][result_json['query']] = result_json['details']
+        elif result_json['action'] == 'end':
+            return result_json['details']
+        else:
+            raise ValueError('Invalid action')
+
+def plan(state: DBFinderState) -> str:
+    """Plans the search for a dataset."""
+    
+    plan_msg = HumanMessage(content=
+        db_finder_plan_search_prompt.format(query=state['query'], temp=" ".join([f"{k}: {v}" for k, v in state['temp'].items()]))
+    )
+    temp = state['messages']
+    temp.append(plan_msg)
+    result = chat.invoke(temp)
+    state['messages'].pop(len(state['messages']) - 1)
+    state['messages'].append(AIMessage(content=result.content.strip()))
+    print("plan: " + result.content.strip())
+    state['messages'].append(HumanMessage(content="Now that we have a plan, let's proceed with the search?"))
+
+    return state
+
 def run_kaggle_api_search(state: DBFinderState) -> str:
     """Searches the Kaggle API via the CLI for datasets matching the user's query."""
 
     # generate the kaggle api search command to be used kaggle datasets -s {query}
-    prompt = ChatPromptTemplate.from_template(
-        "Search the Kaggle API for datasets matching the user's query: {query}"
+    instructions = HumanMessage(content=
+        kaggle_api_search_prompt.format(query=state['query'])  
     )
-    chain = prompt | ChatOpenAI(model="gpt-4o-mini")
-    result = chain.invoke({"query": state['query']})
-    print(result)
-
-
-    state['query'] = 'kaggle datasets -s ' + 'diabetes'
-
+    # temp is created to not add the instuction to messages everytime LLM is called
+    temp = state['messages']
+    temp.append(instructions)
+    result = chat.invoke(temp)
+    state['messages'].pop(len(state['messages']) - 1)
+    state['messages'].append(AIMessage(content="generated command:" + result.content.strip()))
+    print("generated command: " + result.content.strip())
+    count = 0
+    while True:
+        if result.content is not None:
+            state['query'] = result.content.strip()
+            break
+        else:
+            # print(result.content.strip())
+            result = chat.invoke(temp)
+            count += 1
+            if count > 3:
+                raise ValueError('Invalid command')
+    
+    
     # set the query as an env var to be used by image when dockercompose is run
     os.environ['KAGGLE_COMMAND'] = state['query']
 
     # execute the command in a subprocess in a docker container
     try:
         # Run docker-compose up
-        result = subprocess.run(
+        interpreter = subprocess.run(
             ['docker-compose', '-f', 'dockercompose.yaml', 'up', '--build', '--remove-orphans'],
             env=os.environ,
             capture_output=True,
             text=True,
             check=True
         )
-        print(result.stdout)
-        return result.stdout
+        # print(interpreter.stdout)
     except subprocess.CalledProcessError as e:
         print(f"Error running command: {e}")
         print(f"Error output: {e.stderr}")
@@ -54,10 +138,21 @@ def run_kaggle_api_search(state: DBFinderState) -> str:
             capture_output=True
         )
 
-run_kaggle_api_search(
+    # add the result to the messages
+    if interpreter.stdout is not None and interpreter.stdout.split("#6 DONE 0.0s")[1] is not None:
+        # print(interpreter.stdout.split("#6 DONE 0.0s")[1])
+        state['messages'].append(AIMessage(content=interpreter.stdout.split("#6 DONE 0.0s")[1]))
+        # print("search: " + interpreter.stdout.split("#6 DONE 0.0s")[1])
+    else:
+        raise ValueError('No valid result from kaggle api search. There was an error in the command or the command was not executed.')
+    
+    return state
+
+agentic_loop(
     {
         'messages': [],
-        'query': ''
+        'query': 'diabetes in USA',
+        'temp': {}
     }
 )
 
--- prompts.py
+++ prompts.py
@@ -1,1 +1,61 @@
 # STORES ALL THE LARGE PROMPTS
+
+db_finder_loop_prompt =  """
+    You are a helpful assistant that can help with a task of finding a dataset of kaggle based on the user's query {query}. 
+    {"Be sure to plan your approach only once" if len(state['messages']) == 0 else ""}. Be optimal and efficient. 
+
+    You can do the following actions:
+    - plan: plan the search for a dataset
+    - search: use the kaggle api to narrow the search for a dataset
+    - alter: alter the key-values in the temp dict
+    - end: select a dataset (END)
+
+    Here is the info of the temp dict:
+    {temp}
+
+    RETURN IN FOLLOWING FORMAT:
+    {{
+        "action": "plan" | "search" | "alter" | "end",
+        "details": "new query to search for" | "new key-value pair" | "plan for the search" | "the selected dataset's reference"
+        "reason": "reason for the action"
+    }}
+            """
+
+kaggle_api_search_prompt = """ 
+        You are a helpful assistant that creates kaggle api search commands. Search the Kaggle API for datasets to get the most relevant results for the user's query: {query}. 
+        Use previous messages to direct your search, make sure not to re-search the same thing, be optimal. Do not make bash commands, just return the command as a string.
+
+        Here is the usage for the "kaggle datasets list" command:
+        usage: kaggle datasets list [-h] [--sort-by SORT_BY] [--size SIZE] [--file-type FILE_TYPE] [--license LICENSE_NAME]
+                            [--tags TAG_IDS] [-s SEARCH] [-m] [--user USER] [-p PAGE] [-v] [--max-size MAX_SIZE]
+                            [--min-size MIN_SIZE]
+        optional arguments:
+        -h, --help            show this help message and exit
+        --sort-by SORT_BY     Sort list results. Default is 'hottest'. Valid options are 'hottest', 'votes', 'updated', and 'active'
+        --size SIZE           DEPRECATED. Please use --max-size and --min-size to filter dataset sizes.
+        --file-type FILE_TYPE
+                                Search for datasets with a specific file type. Default is 'all'. Valid options are 'all', 'csv', 'sqlite', 'json', and 'bigQuery'. Please note that bigQuery datasets cannot be downloaded
+        --license LICENSE_NAME
+                                Search for datasets with a specific license. Default is 'all'. Valid options are 'all', 'cc', 'gpl', 'odb', and 'other'
+        --tags TAG_IDS        Search for datasets that have specific tags. Tag list should be comma separated
+        -s SEARCH, --search SEARCH
+                                Term(s) to search for
+        -m, --mine            Display only my items
+        --user USER           Find public datasets owned by a specific user or organization
+        -p PAGE, --page PAGE  Page number for results paging. Page size is 20 by default
+        -v, --csv             Print results in CSV format (if not set print in table format)
+        --max-size MAX_SIZE   Specify the maximum size of the dataset to return (bytes)
+        --min-size MIN_SIZE   Specify the minimum size of the dataset to return (bytes)
+
+
+        YOU MUST RETURN AS STRING FORMAT: 
+        kaggle datasets list <your_command>
+        """
+
+db_finder_plan_search_prompt = """
+        You are a helpful assistant that plans the search for a dataset. You have access to the following information:
+        - the user's query: {query}
+        - the temp dict: {temp}
+
+        You also have access to previous messages. You must return a short plan for the search. Return in plain english.
+        """
