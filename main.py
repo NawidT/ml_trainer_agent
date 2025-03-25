256ea4dbad3f35bb828e0f75eda1565c643aa40c
NawidT <nawidtahmid@gmail.com>
2025-03-25 13:52:07 -0400

    file structure

---
+++ main.py
@@ -0,0 +1,6 @@
+# MAIN FILE
+
+
+# handles the planning and eval LLMs
+# connects to the database_finder_agent and code_interpreter_agent
+# stores the chat_history[list: BaseMessage] and key_facts[dict] 
